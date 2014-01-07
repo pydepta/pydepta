23749691574d2fed43645d0acbfd5a09ded94fff
tpeng <pengtaoo@gmail.com>
2014-01-07 17:02:33 +0800

    add DeptaIBLExtractor and DeptaExtractor to extract data from aligned data found by Depta

--- pydepta/depta.py
+++ pydepta/depta.py
@@ -1,12 +1,66 @@
 from urllib import urlopen
-from scrapely import HtmlPage, Scraper, TemplateMaker, best_match
+from scrapely import HtmlPage, Scraper, TemplateMaker, best_match, InstanceBasedLearningExtractor
 from lxml.html import tostring
-
+from scrapely.extraction.regionextract import *
+from scrapely.extraction.similarity import first_longest_subsequence
 from w3lib.encoding import html_to_unicode
 
 from .htmls import DomTreeBuilder
 from .mdr import MiningDataRegion, MiningDataRecord, MiningDataField
 
+class DeptaExtractor(RecordExtractor):
+    """
+    A simple RecordExtractor variant to handle the tabulated data.
+    """
+    def __init__(self, extractors, template_tokens):
+        super(DeptaExtractor, self).__init__(extractors, template_tokens)
+        self.best_match = first_longest_subsequence
+
+    def extract(self, page, start_index=0, end_index=None, ignored_regions=None, **kwargs):
+        if ignored_regions is None:
+            ignored_regions = []
+        region_elements = sorted(self.extractors + ignored_regions, key=lambda x: _labelled(x).start_index)
+        pindex, sindex, attributes = self._doextract(page, region_elements, start_index,
+                                           end_index, **kwargs)
+
+        # collect variant data, maintaining the order of variants
+        items = [(k, v) for k, v in attributes]
+        r = [_attrs2dict(items)]
+
+        if not end_index:
+            end_index = len(page.page_tokens)
+
+        if len(items) > 0 and sindex and sindex < end_index:
+            # extract recursively backward
+            r.extend(self.extract(page, pindex - len(self.template_tokens), pindex, ignored_regions, **kwargs))
+
+        return r
+
+    def __repr__(self):
+        return str(self)
+
+class DeptaIBLExtractor(InstanceBasedLearningExtractor):
+
+    def build_extraction_tree(self, template, type_descriptor, trace=True):
+        """Build a tree of region extractors corresponding to the
+        template
+        """
+        attribute_map = type_descriptor.attribute_map if type_descriptor else None
+        extractors = BasicTypeExtractor.create(template.annotations, attribute_map)
+        if trace:
+            extractors = TraceExtractor.apply(template, extractors)
+        for cls in (DeptaExtractor,):
+            extractors = cls.apply(template, extractors)
+            if trace:
+                extractors = TraceExtractor.apply(template, extractors)
+
+        return TemplatePageExtractor(template, extractors)
+
+    def extract(self, html, pref_template_id=None):
+        extracted, template = super(DeptaIBLExtractor, self).extract(html, pref_template_id)
+        if extracted:
+            return extracted[::-1], template
+        return None, None
 
 class Depta(object):
     def __init__(self, threshold=0.75, k=5):
@@ -74,7 +128,13 @@ class Depta(object):
         doc = builder.build()
         page = HtmlPage(body=tostring(doc, encoding=unicode, method='html'))
 
-        return self.scraper.scrape_page(page)
+        return self._scrape_page(page)
+
+    def _scrape_page(self, page):
+        if self.scraper._ex is None:
+            self.scraper._ex = DeptaIBLExtractor((t, None) for t in
+                self.scraper._templates)
+        return self.scraper._ex.extract(page)[0]
 
     def _region_to_htmlpage(self, region):
         seed_body = tostring(region.parent[region.start], encoding=unicode, method='html')
